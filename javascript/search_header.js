3103c276199492fcbdb1dba78c54ebcc7db5bc40
69_1951061112_HoangThiThuUyen <87602377+HoangUyn8886@users.noreply.github.com>
2021-12-27 22:21:54 +0700

    Update search_header.js

--- javascript/search_header.js
+++ javascript/search_header.js
@@ -3,6 +3,7 @@
 const search = document.querySelector('#search');
 search.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
+        
         window.location = `./process/process-search.php?content_search=${search.value}`;
     }
 });
