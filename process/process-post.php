1e73f9bf50fee8c2d38e4ae8b5d24eeef4d548a4
DinhThiQuynh <94340666+DinhThiQuynh@users.noreply.github.com>
2022-01-02 10:45:40 +0700

    Update process-post.php

--- process/process-post.php
+++ process/process-post.php
@@ -14,26 +14,5 @@ $user_id = $_SESSION['user_id'];
 
 echo "insert into post (title,image,user_id) values ('$title',".$image['name'].",'$user_id')";
 
-//up lên database , chuyển hướng về home
-if (isset($title) && isset($image)) {
-    $sql_up_posts = "insert into post (title,image,user_id) values ('$title','".$image['name']."','$user_id')";
-    if (upImg($image) == true) {
-        mysqli_query($conn, $sql_up_posts);
-    }
-    header('Location: ../home.php');
-} else {
-    if (isset($title) && !isset($image)) {
-        $sql_up_posts = "insert into post (title,user_id) values ('$title','$user_id')";
-        mysqli_query($conn, $sql_up_posts);
-        header('Location: ../home.php');
-    }
-    if (!isset($title) && isset($image)) {
-        $sql_up_posts = "insert into post (image,user_id) values ('".$image['name']."','$user_id')";
-        if (upImg($image) == true) {
-            mysqli_query($conn, $sql_up_posts);
-        }
-        header('Location: ../home.php');
-    }
-}
 
 mysqli_close($conn);
